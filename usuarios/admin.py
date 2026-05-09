8b0bc5d1f345bc9c6a3475dc46aeeec8a8752b14
ivansosabz <ivansosabz@gmail.com>
2026-05-08 22:14:22 -0300

    feat: usuario registra grupo y este se guarda con el usuario que registro el grupo

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Grupo
 
 # Register your models here.
+@admin.register(Grupo)
+class GrupoAdmin(admin.ModelAdmin):
+    list_display = ('nombre','usuario')
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm, GrupoForm
+from django.contrib.auth.decorators import login_required
+
+from .models import Grupo
+
 
 # Create your views here.
 def sign_up(request):
@@ -15,3 +19,28 @@ def sign_up(request):
         'usuarios/signup.html',
         {'form': form}
     )
+
+def grupo(request):
+    grupos = Grupo.objects.all()
+    return render(
+        request,
+        'grupos/grupos.html',
+        {'grupos': grupos}
+    )
+@login_required
+def crear_grupo(request):
+    if request.method == 'POST':
+        form = GrupoForm(request.POST)
+        if form.is_valid():
+            grupo = form.save(commit=False) # sirve para que cree en objeto pero que aun no lo guarde en la bd, porque si guardas usuario dara error
+            grupo.usuario = request.user # el usuario logueado en ese momento
+            grupo.save()
+            return redirect('grupo')
+    else:
+        form = GrupoForm()
+    return render(
+        request,
+        'grupos/crear.html'
+        ,{'form': form}
+    )
+
