cc78bfbb8651b7ff89aa602d090c651db00da663
ivansosabz <ivansosabz@gmail.com>
2026-05-10 18:44:42 -0300

    refactor and fix: modificación de widgets y retoques en editar.html y eliminar.html

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -4,17 +4,33 @@ from django.contrib.auth.models import User
 from .models import Grupo
 
 class UserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Tu email'
+        })
+    )
+
+    #opcional: personalizar widgets, tarea para después
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Tu contraseña'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirma tu contraseña'})
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-
-    #opcional: personalizar widgets, tarea para después
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['email'].widget.attrs.update({'class': 'form-control'})
-    """
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Tu nombre de usuario'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Tu email'
+            }),
+        }
 
 
 class GrupoForm(forms.ModelForm):
@@ -22,8 +38,6 @@ class GrupoForm(forms.ModelForm):
         model = Grupo
         # no ponemos usuarios en fields porque ya se asigna automaticamente
         fields = ('nombre',)
-        """
         widgets = {
             'nombre': forms.TextInput(attrs={'class': 'form-control'}),
         }
-        """
